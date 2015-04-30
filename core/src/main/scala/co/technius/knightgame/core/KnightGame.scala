5f53041fe6ea64df6559fce62cfca05640df7e80
Technius <techniux@gmail.com>
2015-04-29 17:02:24 -0700

    Use varying-fixed timestep

--- core/src/main/scala/co/technius/knightgame/core/KnightGame.scala
+++ core/src/main/scala/co/technius/knightgame/core/KnightGame.scala
@@ -1,9 +1,9 @@
 package co.technius.knightgame.core
 
-import com.badlogic.gdx.{Gdx, Game, Screen}
+import com.badlogic.gdx.{ Gdx, Game, Screen }
 import com.badlogic.gdx.Input.Keys
-import com.badlogic.gdx.graphics.{GL20, OrthographicCamera, Texture}
-import com.badlogic.gdx.graphics.g2d.{SpriteBatch}
+import com.badlogic.gdx.graphics.{ GL20, OrthographicCamera, Texture }
+import com.badlogic.gdx.graphics.g2d.{ BitmapFont, SpriteBatch }
 import com.badlogic.gdx.utils.viewport.FitViewport
 
 import scala.math
@@ -27,7 +27,7 @@ class MainScreen extends Screen {
     Keys.D -> Controls.Move(Direction.Right),
     Keys.SPACE -> Controls.Stab
   )
-  
+
   camera.setToOrtho(false, 100, 100)
 
   var player = Player()
@@ -44,12 +44,12 @@ class MainScreen extends Screen {
     batch.end()
 
     updateTime += deltaTime
-    if (updateTime >= 1f/60f) {
-      updateTime = 0f
+    while (updateTime >= 1f / 60f) {
+      updateTime -= 1f / 60f
       val pressed = controls filter { t =>
         Gdx.input.isKeyPressed(t._1)
       } map (_._2)
-      player = Logic(player, deltaTime).input(pressed).actions.player
+      player = Logic(player, 1f / 60f).input(pressed).actions.player
     }
   }
 
